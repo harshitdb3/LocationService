df82b96a093110fb841c1656119ef90e4fa67958
HarshitUpsurge <Harshitdb3@gail.com>
2023-04-15 15:14:45 +0530

    get_users POST request fixed

--- src/main/java/com/location/Location/UpdateTable.java
+++ src/main/java/com/location/Location/UpdateTable.java
@@ -16,15 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
   @Autowired
   private NamedParameterJdbcTemplate jdbcTemplate;
 
-  public void insertData(String name, double latitude, double longitude ) {
-    String sql = "INSERT INTO user_location (name,latitude,longitude) VALUES (:name,:latitude,:longitude)";
+  public void insertData(String name, double latitude, double longitude ) 
+  {
+  String sql = "INSERT INTO user_location (name,latitude,longitude) VALUES (:name,:latitude,:longitude)";
   Map<String, Object> params = new HashMap<>();
   params.put("name",name);  
   params.put("latitude",latitude);
   params.put("longitude",longitude);
   int rowsAffected = jdbcTemplate.update(sql, params);
   System.out.println(rowsAffected + " rows inserted.");
-}
+  }
+  
 	@PostMapping("/update_data")
 	public ResponseEntity<Void> updatetable(@RequestParam("name") String name,
   @RequestParam("latitude") double latitude,
---
+++ src/main/java/com/location/Location/UserLocation.java
@@ -0,0 +1,42 @@
+package com.location.Location;
+
+public class UserLocation {
+
+
+  
+  private String name;
+  private double latitude;
+  private double longitude;
+
+  
+  public String getName() {
+    return this.name;
+  }
+
+
+  public double getLatitude() {
+    return this.latitude;
+  }
+
+
+  public double getLongitude() {
+    return this.longitude;
+  }
+
+  public void setName(String string) 
+  {
+  name = string;
+  }
+  public void setLatitude(Double double1) 
+  {
+  latitude = double1;
+  }
+  public void setLongitude(Double double1) 
+  {
+  longitude = double1;
+  }
+
+
+ 
+
+}
